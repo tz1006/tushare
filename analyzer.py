c34494f6e640cf7b5049475b9387bb7fcf6b102b
tz1006 <a@omg.tf>
2018-10-28 14:57:43 +0800

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -6,16 +6,56 @@
 
 
 import tushare as ts
+from datetime import datetime, timedelta
+
+
+def datechange(date, change):
+    d = datetime.strptime(date,'%Y-%m-%d').date()
+    new_d = d + timedelta(days=change)
+    new_date = new_d.strftime('%Y-%m-%d')
+    return new_date
+
+
+import futuquant as ft
+
+q = ft.OpenQuoteContext(host='sz.omg.tf', port=11111)
+# 沪深A股
+hs = q.get_plate_stock('SH.3000005')[1]
+q.close()
+hs['code'] = hs['code'].map(lambda x: x[3:])
+# 富途牛牛Code
+ftid = hs.set_index('code').drop(columns=['stock_owner','stock_name', 'lot_size', 'stock_child_type', 'stock_type', 'list_time']).T.to_dict('records')[0]
+
+
+
+
+
 
 
 # MA5 MA10 某日交叉后走势
 
 code = '600123'
-select_date = '2018-09-10'
-start_date = '2018-09-05'
-end_date = '2018-09-15'
+select_date = '2018-05-09'
+start_date = datechange(select_date, -20)
+end_date = datechange(select_date, 8)
 
-data = ts.get_hist_data('600123',start='2018-09-05',end='2018-09-15')
+data = ts.get_hist_data(code,start=start_date,end=end_date)
+d0 = data.loc[:select_date].index[-2]
+hist_data = data.loc[d0:][['close', 'ma5', 'ma10']]
+
+last_close = hist_data.iloc[1]['close']
+ma4 = hist_data[1:5]['close'].mean()
+ma9 = hist_data[1:10]['close'].mean()
+d0ma5p = (ma4 * 4 + last_close * 1.04) / 5
+d0ma10p = (ma9 * 9 + last_close * 1.04) / 10
+d1ma5 = hist_data.iloc[1]['ma5']
+d1ma10 = hist_data.iloc[1]['ma10']
+d2ma5 = hist_data.iloc[2]['ma5']
+d3ma5 = hist_data.iloc[3]['ma5']
+d0ma5 = hist_data.iloc[0]['ma5']
+d0ma10 = hist_data.iloc[0]['ma10']
+
+
 futu_data = data.loc[:'2018-09-10'][['close', 'ma5', 'ma10']]
 hist_data = data.loc['2018-09-10':][['close', 'ma5', 'ma10']]
 last_close = hist_data.iloc[0]['close']
@@ -32,12 +72,13 @@ d3ma5 = hist_data.iloc[2]['ma5']
 if (d1ma5 < d1ma10) and (d3ma5 < d2ma5 < d1ma5):
     
 
-
+# model1('600123', '2018-05-09')
 def model1(code, select_date):
     start_date = datechange(select_date, -20)
     end_date = datechange(select_date, 8)
-    data = ts.get_hist_data('600123',start=start_date,end=end_date)
+    data = ts.get_hist_data(code, start=start_date,end=end_date)
     hist_data = data.loc[select_date:][['close', 'ma5', 'ma10']]
+    #print(hist_data)
     last_close = hist_data.iloc[0]['close']
     ma4 = hist_data['close'][0:4].mean()
     ma9 = hist_data['close'][0:9].mean()
@@ -47,7 +88,9 @@ def model1(code, select_date):
     d1ma10 = hist_data.iloc[0]['ma10']
     d2ma5 = hist_data.iloc[1]['ma5']
     d3ma5 = hist_data.iloc[2]['ma5']
+    # print('LAST_CLOSE: %s \nMA4: %s \nMA9: %s \nd0MA5: %s \nd0MA10: %s \nd1MA5: %s \nd1MA10: %s \nd2MA5: %s \nd3MA5: %s' % (last_close, ma4, ma9, d0ma5, d0ma10, d1ma5, d1ma10, d2ma5, d3ma5))
     if (d1ma5 < d1ma10) and (d3ma5 < d2ma5 < d1ma5) and (d0ma5 > d0ma10):
+        #print(True)
         d0 = data.loc[:select_date].index[-2]
         futu_data = data.loc[:d0]
         close = futu_data['close'].mean()
@@ -59,6 +102,7 @@ def model1(code, select_date):
         result = [(close, close_ratio), (high, high_ratio), (low, low_ratio)]
     else:
         result = None
+    return result
 
 
 dd = {}
@@ -70,9 +114,9 @@ for i in tqdm(ftid):
 
 
 def analyze_model1(code):
-    print(code)
-    r =  model1(code, '2018-09-05')
-    dd[i] = r
+    #print(code)
+    r =  model1(code, '2018-05-09')
+    dd[code] = r
 
 
 
@@ -98,5 +142,159 @@ def t_analyze_model1():
             pass
 
 
+def printdd():
+    li = {}
+    for i in dd:
+        r = dd[i]
+        if r != None:
+            li[i] = r
+    close_win = 0
+    for x in li:
+        rr = li[x]
+        close_ratio = rr[0][1]
+        if close_ratio > 0.01:
+            close_win += 1
+        print(x)
+        print(rr)
+    print(len(li))
+    print('close_win:')
+    print(close_win)
 
 
+
+
+class model1():
+    def __init__(self, date, rising_ratio=1.04):
+        self.date = date
+        self.rising_ratio = rising_ratio
+        self.dd = {}
+    def model1(self, code, select_date, rising_ratio):
+        start_date = datechange(select_date, -20)
+        end_date = datechange(select_date, 8)
+        data = ts.get_hist_data(code, start=start_date,end=end_date)
+        hist_data = data.loc[select_date:][['close', 'ma5', 'ma10']]
+        #print(hist_data)
+        last_close = hist_data.iloc[0]['close']
+        ma4 = hist_data['close'][0:4].mean()
+        ma9 = hist_data['close'][0:9].mean()
+        d0ma5 = (ma4 * 4 + last_close * rising_ratio) / 5
+        d0ma10 = (ma9 * 9 + last_close * rising_ratio) / 10
+        d1ma5 = hist_data.iloc[0]['ma5']
+        d1ma10 = hist_data.iloc[0]['ma10']
+        d2ma5 = hist_data.iloc[1]['ma5']
+        d3ma5 = hist_data.iloc[2]['ma5']
+        # print('LAST_CLOSE: %s \nMA4: %s \nMA9: %s \nd0MA5: %s \nd0MA10: %s \nd1MA5: %s \nd1MA10: %s \nd2MA5: %s \nd3MA5: %s' % (last_close, ma4, ma9, d0ma5, d0ma10, d1ma5, d1ma10, d2ma5, d3ma5))
+        if (d1ma5 < d1ma10) and (d3ma5 < d2ma5 < d1ma5) and (d0ma5 > d0ma10):
+            #print(True)
+            d0 = data.loc[:select_date].index[-2]
+            futu_data = data.loc[:d0]
+            close = futu_data['close'].mean()
+            close_ratio = close / last_close - 1
+            high = futu_data['high'].max()
+            high_ratio = high / last_close - 1
+            low = futu_data['low'].min()
+            low_ratio = low / last_close - 1
+            result = [(close, close_ratio), (high, high_ratio), (low, low_ratio)]
+        else:
+            result = None
+        return result
+    def analyze_model1(self, code, date, rising_ratio):
+        #print(code)
+        r = self.model1(code, date, rising_ratio)
+        self.dd[code] = r
+    def analyze(self):
+        futures = []
+        with ThreadPoolExecutor(max_workers=200) as executor:
+            for i in ftid:
+                futures.append(executor.submit(self.analyze_model1, i, self.date, self.rising_ratio))
+            kwargs = {'total': len(futures)}
+            for f in tqdm(as_completed(futures), **kwargs):
+                pass
+    def printresult(self):
+        li = {}
+        for i in self.dd:
+            r = dd[i]
+            if r != None:
+                li[i] = r
+        close_win = 0
+        for x in li:
+            rr = li[x]
+            close_ratio = rr[0][1]
+            if close_ratio > 0.01:
+                close_win += 1
+            print(x)
+            print(rr)
+        print(len(li))
+        print('close_win:')
+        print(close_win)
+    
+    
+# a = model('2018-05-09')
+class model():
+    def __init__(self, date, rising_ratio=1.04):
+        self.date = date
+        self.rising_ratio = rising_ratio
+        self.dd = {}
+    def model2(self, code, select_date, rising_ratio):
+        start_date = datechange(select_date, -20)
+        end_date = datechange(select_date, 8)
+        data = ts.get_hist_data(code, start=start_date,end=end_date)
+        d0 = data.loc[:select_date].index[-2]
+        hist_data = data.loc[d0:][['close', 'ma5', 'ma10', 'high']]
+        last_close = hist_data.iloc[1]['close']
+        d0high = hist_data.iloc[0]['high']
+        ma4 = hist_data[1:5]['close'].mean()
+        ma9 = hist_data[1:10]['close'].mean()
+        d0ma5p = (ma4 * 4 + last_close * 1.04) / 5
+        d0ma10p = (ma9 * 9 + last_close * 1.04) / 10
+        d0ma5h = (ma4 * 4 + d0high) / 5
+        d0ma10h = (ma9 * 9 + d0high) / 10
+        d1ma5 = hist_data.iloc[1]['ma5']
+        d1ma10 = hist_data.iloc[1]['ma10']
+        d2ma5 = hist_data.iloc[2]['ma5']
+        d3ma5 = hist_data.iloc[3]['ma5']
+        # print('LAST_CLOSE: %s \nMA4: %s \nMA9: %s \nd0MA5: %s \nd0MA10: %s \nd1MA5: %s \nd1MA10: %s \nd2MA5: %s \nd3MA5: %s' % (last_close, ma4, ma9, d0ma5, d0ma10, d1ma5, d1ma10, d2ma5, d3ma5))
+        if (d1ma5 < d1ma10) and (d3ma5 < d2ma5 < d1ma5) and (d0ma5p > d0ma10p) and (d0ma5h > d0ma10h):
+            #print(True)
+            futu_data = data.loc[:d0]
+            even_pirce = ma9 * 9 - ma4 * 8
+            even_ratio = even_pirce / last_close - 1
+            high = futu_data['high'].max()
+            high_ratio = high / last_close - 1
+            low = futu_data['low'].min()
+            low_ratio = low / last_close - 1
+            result = [(even_pirce, even_ratio), (high, high_ratio), (low, low_ratio)]
+        else:
+            result = None
+        return result
+    def analyze_model2(self, code, date, rising_ratio):
+        #print(code)
+        r = self.model2(code, date, rising_ratio)
+        self.dd[code] = r
+    def analyze(self):
+        futures = []
+        with ThreadPoolExecutor(max_workers=200) as executor:
+            for i in ftid:
+                futures.append(executor.submit(self.analyze_model2, i, self.date, self.rising_ratio))
+            kwargs = {'total': len(futures)}
+            for f in tqdm(as_completed(futures), **kwargs):
+                pass
+    def printresult(self):
+        li = {}
+        for i in self.dd:
+            r = self.dd[i]
+            if r != None:
+                li[i] = r
+        close_win = 0
+        for x in li:
+            rr = li[x]
+            close_ratio = rr[0][1]
+            if close_ratio > 0.01:
+                close_win += 1
+            print(x)
+            print(rr)
+        print(len(li))
+        print('close_win:')
+        print(close_win)
+    
+        
